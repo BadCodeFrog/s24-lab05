15359dde9610e0146481cd51cb03d0788ae9cccc
Yibo Ma <yibo@YibodeMacBook-Pro.local>
2024-02-13 20:36:23 -0500

    Task 1: Instrumentation with inheritance, The addAll() in SortedIntList actually do the add method again. So that we don’t need to add the totalAdded second time.

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -6,11 +6,51 @@ package edu.cmu.cs.cs214.rec04;
  * with the current size, which goes down when an element is removed)
  * and exports an accessor (totalAdded) for this count.
  *
- * @author Nora Shoemaker
+ * @author Yibo Ma(yibom)
  *
  */
 
-public class InheritanceSortedIntList {
+public class InheritanceSortedIntList extends SortedIntList {
     // Write your implementation below with API documentation
+    private int totalAdded;
+
+    /**
+     * Constructor for InheritanceSortedIntList
+     */
+    public InheritanceSortedIntList() {
+        super();
+        totalAdded = 0;
+    }
+
+    /**
+     * Overridden add method to keep track of the totalAdded
+     */
+    @Override
+    public boolean add(int num) {
+        totalAdded++;
+        return super.add(num);
+    }
+
+    /**
+     * Overridden addAll method to keep track of the totalAdded
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        //InheritanceSortedIntList newlist = (InheritanceSortedIntList) list;
+        // totalAdded += list.size();
+        return super.addAll(list);
+    }
+
+    /**
+     * Accessor for the totalAdded
+     * @return the totalAdded
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+    // public int size() {
+    //     return super.size();
+    // }
 
 }
